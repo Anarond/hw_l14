e04e113214ad6f58672f8fd95d93c8a218bf1b2c
aleksandr kuznetsov <anarond@gmail.com>
2026-02-01 12:44:51 +0300

    back to selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,27 +14,28 @@ def load_env():
 
 @pytest.fixture
 def browser_set():
-    # selenoid_login = os.getenv("SELENOID_LOGIN")
-    # selenoid_pass = os.getenv("SELENOID_PASS")
-    # selenoid_url = os.getenv("SELENOID_URL")
-    #
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    #
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
 
-    driver = browser.driver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+#    driver = browser.driver
+
     browser.config.base_url = 'https://ecommerce-playground.lambdatest.io'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
